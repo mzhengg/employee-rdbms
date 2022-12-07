1e5283d67982e262c127f74f80e029577fbcfe44
Michael Zheng <99830293+mzhengg@users.noreply.github.com>
2022-12-07 16:32:52 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,147 @@
+# Employee Database Management System
+
+Data modeling is an important job of a Data Engineer. So, in this project, I designed a RDBMS (Relational Database Management System) for employee management applications to practice this skill.
+
+## Database Principles
+
+A brief overview of important database concepts
+
+### 1) Overview
+Data modeling is the process of creating a visual representation of either a whole information system or parts of it to communicate connections between data points and structures
+
+Goal is to illustrate types of data used and stored, relationships among these data types, and the ways the data can be grouped and organized and its formats and attributes
+ 
+Data models built around business needs. Rules and requirements defined upfront through feedback from business stakeholders so they can be incorporated into the design of a new system or adapted into an existing one
+ 
+Benefits of data modeling:
+* Provides project scope
+* Acts as documentation
+* Improves performance
+* Reduces data and application errors
+ 
+Types of data models: (hierarchy)
+* Conceptual data models:
+    * Establish the entities and their relationships
+* Logical data models
+    * Define the attributes and elaborate on their relationships
+        * Ex. Entity Relationship diagram (ERD), Key-Based model (KB), Fully-Attributed model (FA)
+* Physical data models
+    * Describes the database-specific implementation of the data model
+ 
+Data modeling process:
+* Identify the entities of business importance
+* Identify key attributes of each entity
+* Identify relationships among entities
+* Map attributes to entities completely
+* Assign keys as needed and decide on a degree of normalization (that balances the need to reduce redundancy with performance requirements)
+* Finalize and validate the data model
+
+### 2) Relational Databases
+
+Relational Database: a type of database that stores and provides access to data points that are related to one another
+ 
+RDBMS: The software used to store, manage, query, and retrieve data stored in a relational database
+ 
+Data Integrity: having correct data in database (no repeating or incorrect values and broken relationships)
+* Entity integrity:  each row of a table has a unique and non-null primary key
+* Referential integrity: each value in the foreign key must have a matching value in the primary key or it must be null
+* Domain integrity: all data values in each column must be valid (proper data type and length)
+ 
+Relationships: associations between entities (tables)
+* One-to-one: a record in one table has a connection to a single record in another table
+* One-to-many: a record in one table has connections to multiple records in another table
+* Many-to-many: multiple records in one table have connections with multiple records in another table (incompatible with relational databases)
+ 
+Parent tables: contain the primary key
+ 
+Child tables: contain the foreign key
+A child table always points back at the parent table
+ 
+Look-up table: a table that has two columns: "key" and "value". The keys are usually integers or short string codes. The keys are being used by other tables. Thus, we can quickly search data because the lookup keys are connected to all the corresponding rows
+ 
+Cardinality: the maximum number of times a row in one table can relate to the rows in another table
+ 
+Modality: the minimum number of times a row in one table can relate to the rows in another table
+
+### 3) Schemas
+Structured data: made up of well-defined data types with patterns that make them easily searchable
+ 
+Unstructured data: made up of files in various formats, such as videos, photos, texts, audio, and more
+ 
+Data engineers collect unstructured data, transform it into structured data, and store it in database management systems (DBMS) to make it available for analysis
+ 
+A schema is a collection of database objects
+
+There are a variety of ways to arrange schema objects in the schema models designed for data warehousing:
+* Star Schema: (the simplest)
+    * Design: contains a fact table at the center connected to a number of associated dimensional tables
+        * Fact table: contains all the primary keys of the dimensional tables and stores quantitative information for analysis
+        * Dimensional tables: provide descriptive information for all the quantitative information in the fact table (thus dimensional tables are relatively smaller than fact tables). Commonly used dimensions: people, products, place, and time
+
+<img>
+
+* Snowflake Schema: (variant of Star Schema)
+    * Design: contains a fact table at the center connected to a number of associated dimensional tables that have their own sub-dimensional tables
+
+<img>
+
+* Galaxy Schema:
+    * Design: contains two fact tables that share dimensional tables between them and have their own dimensional tables
+
+<img>
+
+
+
+
+### 4) Keys
+
+Natural key: a set of attributes that already exist in the table that can be used to make keys (see below)
+ 
+Surrogate key: a system generated attribute with no business meaning that can be used to make keys (see below)
+ 
+* Super key: a set of attributes (can include unnecessary attributes) that can uniquely identify each row in a table (there can be many Super keys in a table)
+ 
+* Candidate key: a subset of Super keys devoid of any unnecessary attributes that are not important for uniquely identifying each row in a table (there can be many Candidate keys in a table)
+ 
+* Primary key: Out of all the Candidate keys, we pick the one that has a minimal, non-null set of attributes that can uniquely identify each row in a table (there can only be one Primary key in a table)
+ 
+* Alternate key: Any of the remaining Candidate keys
+ 
+* Foreign key: an attribute that is a Primary key in its parent table, but is included as an attribute in the child table to generate a relationship between them (each value in the foreign key must correspond to a value in the primary key otherwise it should be null)
+ 
+Simple key: a key made up of one column
+ 
+Composite key: a key made up of two or more columns
+ 
+Compound key: a key made up of two or more foreign keys from different tables
+
+### 5) Normalization
+
+Partial dependency: when a non-key column in a table is not dependent on the entire primary key
+ 
+Functional dependency: when a non-key column in a table is dependent on the entire primary key
+ 
+Transitive dependency: when a column is dependent on another column through a dependency (A -> C because A -> B where B -> C) and causes a FD
+
+Normalization: the process by which data in a relational database is organized to eliminate data redundancy by removing all model structures that provide multiple ways to know the same fact
+* First Normal Form:
+    * All column values are atomic
+* Second Normal Form:
+    * It is in 1NF
+    * No partial dependencies, otherwise the column(s) should be moved to another table
+* Third Normal Form:
+    * It is in 2NF
+    * No transitive dependencies, otherwise the column(s) should be moved to another table
+ 
+Denormalization: the process of adding precomputed redundant data to an otherwise normalized relational database to improve read performance of the database
+
+### 6) Index
+
+Non-clustered index: list of indices where each index points to all the corresponding rows
+ 
+Clustered index: list of indices where each index points to a block containing all the corresponding rows (actually reorganizes the data)
+
+## Technologies
+
+* MySQL
+* Docker
---
+++ containers/interface/interface.py
@@ -0,0 +1,503 @@
+import mysql.connector
+from mysql.connector import Error
+
+# login information
+hostname = "localhost"
+database = "Project"
+mysql_user = "admin"
+mysql_password = "admin"
+
+# mysql connection
+connection = mysql.connector.connect(host=hostname, user=mysql_user, passwd=mysql_password, auth_plugin='mysql_native_password', database=database)
+
+def interface():
+    i = 1
+    while i != 0:
+        print("Please select an operation to perform.")
+        print()
+        print("A - Quit Menu")
+        print("B - Add employee")
+        print("C - View employee")
+        print("D - Modify employee")
+        print("E - Remove employee")
+        print("F - Add dependent")
+        print("G - Remove dependent")
+        print("H - Add department")
+        print("I - View department")
+        print("J - Remove department")
+        print("K - Add department location")
+        print("L - Remove department location")
+        print()
+
+        user_input = input("Enter the operation letter: ")
+
+        if user_input == "A":
+            print("Closing menu")
+            i = 0
+        elif user_input == "B":
+            add_employee()
+
+        elif user_input == "C":
+            view_employee()
+
+        elif user_input == "D":
+            modify_employee()
+
+        elif user_input == "E":
+            remove_employee()
+
+        elif user_input == "F":
+            add_dependent()
+
+        elif user_input == "G":
+            remove_dependent()
+
+        elif user_input == "H":
+            add_department()
+
+        elif user_input == "I":
+            view_department()
+
+        elif user_input == "J":
+            remove_department()
+
+        elif user_input == "K":
+            add_department_location()
+            
+        elif user_input == "L":
+            remove_department_location()
+        else:
+            print("This is not a valid input")
+            i = 0
+
+        print()
+        ans = input("Do you want to perform another operation? 'Yes' or 'No' ")
+
+        if ans == "Yes":
+            continue
+        elif ans == "No":
+            print("Closing menu")
+            i = 0
+        else:
+            i = 0
+            print("Not a valid input. Closing menu.")
+
+def add_employee():
+    cursor = connection.cursor()
+
+    ssn = input("Enter employee SSN: ")
+    fname, minit, lname = input("Enter employee full name: (first, middle, last) ").split()
+    bdate = input("Enter employee birthdate: ")
+    address = input("Enter employee address: ")
+    sex = input("Enter employee sex: ")
+    salary = input("Enter employee salary: ")
+    super_ssn = input("Enter employee supervisor's SSN: (If none, then enter 'NULL') ")
+    
+    if super_ssn == 'NULL':
+        super_ssn = None
+
+    dno = input("Enter the employee's department number: ")
+
+    try:
+        data = (fname, minit, lname, ssn, bdate, address, sex, salary, super_ssn, dno)
+        # insert new employee into table
+        cursor.execute("INSERT INTO Employee VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", data)
+        print("Employee successfully added!")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    connection.commit()
+    cursor.close()
+
+def view_employee():
+    cursor = connection.cursor()
+
+    ssn = input("Enter employee SSN: ")
+
+    # select all attributes of employee with that ssn
+    cursor.execute("SELECT * FROM Employee WHERE Ssn = '%s'" % (ssn))
+    emp_info = cursor.fetchall()
+
+    if len(emp_info) == 0:
+        return print("This employee does not exist!")
+
+    # select supervisor's full name
+    super_ssn = emp_info[0][8]
+    cursor.execute("SELECT Fname, Minit, Lname FROM Employee WHERE Ssn = '%s'" % (super_ssn))
+    super_name = cursor.fetchall()
+
+    if len(super_name) == 0:
+        super_name = "NULL"
+        super_ssn = "NULL"
+    else:
+        super_name = " ".join(super_name[0])
+
+    # select department name
+    dno = emp_info[0][9]
+    cursor.execute("SELECT Dname FROM Department WHERE Dnumber = '%s'" % (dno))
+    dname = cursor.fetchall()
+
+    # select dependent's name 
+    cursor.execute("SELECT Dependent_name FROM Dependent WHERE Essn = '%s'" % (ssn))
+    dep_names = cursor.fetchall()
+
+    # display all the information
+    if len(dep_names) > 1:
+        dep_names = [entry[0] for entry in dep_names]
+        dep_names = ", ".join(dep_names)
+    elif len(dep_names) == 1:
+        dep_names = dep_names[0][0]
+    else: 
+        dep_names = "NULL"
+
+    print("Employee information: ")
+    print("Name: " + emp_info[0][0] + " " + emp_info[0][1] + " " + emp_info[0][2])
+    print("SSN: " + emp_info[0][3])
+    print("Birthdate: " + str(emp_info[0][4]))
+    print("Address: " + emp_info[0][5])
+    print("Sex: " + emp_info[0][6])
+    print("Salary: " + str(emp_info[0][7]))
+    print("Supervisor's SSN: " + super_ssn)
+    print("Department number: " + str(dno))
+    print("Supervisor name: " + super_name)
+    print("Department name: " + dname[0][0])
+    print("Dependent names: ", dep_names)
+
+    connection.commit()
+    cursor.close()
+
+def modify_employee():
+    cursor = connection.cursor()
+
+    ssn = input("Enter employee SSN: ")
+
+    # select employee information and lock the record
+    cursor.execute("SELECT * FROM Employee WHERE Ssn = '%s' FOR UPDATE" % (ssn))
+    emp = cursor.fetchall()
+
+    print("Employee information: ")
+    print("Name: " + emp[0][0] + " " + emp[0][1] + " " + emp[0][2])
+    print("SSN: " + emp[0][3])
+    print("Birthdate: " + str(emp[0][4]))
+    print("Address: " + emp[0][5])
+    print("Sex: " + emp[0][6])
+    print("Salary: " + str(emp[0][7]))
+    print("Supervisor's SSN: " + str(emp[0][8]))
+    print("Department number: " + str(emp[0][9]))
+
+    i = 1
+
+    while i != 0:
+        print("Options: ")
+        print("A - Quit")
+        print("B - Address")
+        print("C - Sex")
+        print("D - Salary")
+        print("E - Supervisor's SSN")
+        print("F - Department number")
+
+        ans = input("Which attribute do you want to update? Input the appropriate letter: ")
+
+        if ans == "A":
+            return
+        elif ans == "B":
+            address = input("Enter the new address: ")
+            # update employee's address to the new address
+            cursor.execute("UPDATE Employee SET address = '%s' WHERE ssn = '%s'" % (address, ssn))
+            print("Address successfully updated!")
+        elif ans == "C":
+            sex = input("Enter the new sex: ")
+            # update employee's sex to the new sex 
+            cursor.execute("UPDATE Employee SET sex = '%s' WHERE ssn = '%s'" % (sex, ssn))
+            print("Sex successfully updated!")
+        elif ans == "D":
+            salary = input("Enter the new salary: ")
+            # update employee's salary to the new salary 
+            cursor.execute("UPDATE Employee SET salary = '%s' WHERE ssn = '%s'" % (salary, ssn))
+            print("Salary successfully updated!")
+        elif ans == "E":
+            super_ssn = input("Enter the new supervisor SSN: ")
+            try:
+                # update employee's super_ssn to the new super_ssn
+                cursor.execute("UPDATE Employee SET Super_ssn = '%s' WHERE ssn = '%s'" % (super_ssn, ssn))
+                print("Supervisor SSN successfully updated!")
+            except Error as err:
+                print(f"Error: '{err}'")
+        elif ans == "F":
+            dno = input("Enter the new department number: ")
+            try:
+                # update employee dno to the new dno 
+                cursor.execute("UPDATE Employee SET dno = '%s' WHERE ssn = '%s'" % (dno, ssn))
+                print("Department number successfully updated!")
+            except Error as err:
+                print(f"Error: '{err}'")
+        else:
+            return print("Error: Please input a valid option!")
+        
+        connection.commit()
+        inp = input("Are there any other attributes you want to update? Type 'Yes' or 'No' ")
+        
+        if inp == "Yes":
+            continue
+        elif inp == "No":
+            i = 0
+
+    cursor.close()
+
+def remove_employee():
+    cursor = connection.cursor()
+
+    ssn = input("Enter employee SSN: ")
+
+    # retrieve employee information with that ssn and lock the row
+    cursor.execute("SELECT * FROM Employee WHERE Ssn='%s' FOR UPDATE" % (ssn))
+    emp_info = cursor.fetchall()
+
+    try:
+        print("Employee information: ")
+        print("Name: " + emp_info[0][0] + " " + emp_info[0][1] + " " + emp_info[0][2])
+        print("SSN: " + emp_info[0][3])
+        print("Birthdate: " + str(emp_info[0][4]))
+        print("Address: " + emp_info[0][5])
+        print("Sex: " + emp_info[0][6])
+        print("Salary: " + str(emp_info[0][7]))
+
+        conf = input("Are you sure you want to delete this employee? Type 'Yes' or 'No'")
+        if conf == "No":
+            pass
+        else:
+            try:
+                # delete employee with the given ssn from employee table
+                cursor.execute("DELETE FROM Employee WHERE Ssn='%s'" % (ssn))
+                print("Employee successfully removed!")
+            except Error as err:
+                print(f"Error: '{err}'")
+                print("Please remove the necessary dependencies first!")
+    except:
+        return print("This employee does not exist!")
+
+    connection.commit()
+    cursor.close()
+    
+def add_dependent():
+    cursor = connection.cursor()
+
+    ssn = input("Enter Employee SSN: ")
+
+    # check if employee ssn exists
+    cursor.execute("SELECT * FROM Employee WHERE Ssn='%s'", ssn)
+    emp = cursor.fetchall()
+    if len(emp) == 0:
+        return print("This employee does not exist!")
+
+    # selects all dependents assosicated with that ssn and lock the row
+    cursor.execute("SELECT * FROM Dependent WHERE Essn='%s' FOR UPDATE" % (ssn))
+    dep = cursor.fetchall()
+    
+    if len(dep) == 0:
+        return print("This employee does not have any dependents!")
+    else:
+        print("Dependent information: ")
+        for t in dep:
+            print("Name: " + t[1])
+            print("Sex: " + t[2])
+            print("Birthdate: " + str(t[3]))
+            print("Relationship: " + t[4])
+            print()
+
+    # adds new dependent information to dependent table
+    dep_name = input("Enter the dependent's first name: ")
+    sex = input("Enter the dependent's sex: ")
+    bdate = input("Enter the dependent's birthdate: (YYYY-MM-DD) ")
+    relationship = input("Enter the dependent's relationship to the employee: ")
+    
+    data = (ssn, dep_name, sex, bdate, relationship)
+    # check if the dependent already exists in the table
+    cursor.execute("SELECT * FROM Dependent WHERE Essn='%s' AND Dependent_name='%s' AND Sex='%s' AND Bdate='%s' AND Relationship='%s'" % (data))
+
+    if len(cursor.fetchall()) > 0:
+        return print("This dependent already exists in the table!")
+    else:
+        # since they are not in the table, add the dependent to the table
+        cursor.execute("INSERT INTO Dependent VALUES (%s, %s, %s, %s, %s)", data)
+        print("Dependent successfully added!")
+
+    connection.commit()
+    cursor.close()
+
+def remove_dependent():
+    cursor = connection.cursor()
+
+    ssn = input("Enter employee SSN: ")
+
+    # selects all dependents assosicated with that ssn
+    cursor.execute("SELECT * FROM Dependent WHERE Essn='%s' FOR UPDATE" % (ssn))
+    dep = cursor.fetchall()
+
+    if len(dep) == 0:
+        return print("This employee does not have any dependents!")
+    else:
+        print("Dependent information: ")
+        for t in dep:
+            print("Name: " + t[1])
+            print("Sex: " + t[2])
+            print("Birthdate: " + str(t[3]))
+            print("Relationship: " + t[4])
+            print()
+
+    dep_name = input("Enter the first name of the dependent to be removed: ")
+
+    # check if dependent exists
+    cursor.execute("SELECT * FROM Dependent WHERE Essn = '%s' AND Dependent_name = '%s'" % (ssn, dep_name))
+    if len(cursor.fetchall()) == 0:
+        return print("This dependent does not exist in the table!")
+    else:
+        # delete the dependent from the dependent table
+        cursor.execute("DELETE FROM Dependent WHERE Dependent_name = '%s'" % (dep_name))
+        print("Dependent successfully removed!")
+
+    connection.commit()
+    cursor.close()
+
+def add_department():
+    cursor = connection.cursor()
+
+    dname = input("Enter the department name: ")
+    dnumber = input("Enter the department number: ")
+    mgr_ssn = input("Enter the manager SSN: ")
+    mgr_start_date = input("Enter the manager start date: (YYYY-MM-DD) ")
+    
+    try:
+        data = (dname, dnumber, mgr_ssn, mgr_start_date)
+        cursor.execute("INSERT INTO Department VALUES ('%s','%s','%s','%s')" % (data))
+        print("Department successfully added!")
+    except Error as err:
+        print(f"Error: '{err}'")
+
+    connection.commit()
+    cursor.close()
+
+def view_department():
+    cursor = connection.cursor()
+
+    dnumber = input("Enter department number: ")
+
+    # select department information with the given department number
+    cursor.execute("SELECT Dname, Mgr_ssn FROM Department WHERE Dnumber = '%s'" % (dnumber))
+    dept_info = cursor.fetchall()
+    
+    if len(dept_info) == 0:
+        return print("This is an invalid department number!")
+    
+    # select the department supervisor's name 
+    mgr_ssn = dept_info[0][1]
+    cursor.execute("SELECT Fname, Minit, Lname FROM Employee WHERE Ssn = '%s'" % (mgr_ssn))
+    mgr_info = cursor.fetchall()
+
+    # statement the location(s) of that department
+    cursor.execute("SELECT Dlocation FROM Dept_locations WHERE Dnumber = '%s'" % (dnumber))
+    dept_loc = cursor.fetchall()
+
+    # make it look nicer
+    print()
+    print("Department information: ")
+    print("Department: " + dept_info[0][0])
+    print("Department manager: " + " ".join(mgr_info[0]))
+    print("Department location(s): " + " ".join(dept_loc[0]))
+
+    connection.commit()
+    cursor.close()
+
+def remove_department():
+    cursor = connection.cursor()
+
+    dnumber = input("Enter department number: ")
+
+    try:
+        # retrieve information about the department with that department number and lock the row
+        cursor.execute("SELECT * FROM Department WHERE Dnumber='%s' FOR UPDATE" % (dnumber))
+        emp_info = cursor.fetchall()
+        print("Department information: ")
+        print("Department name: " + emp_info[0][0])
+        print("Department number: " + str(emp_info[0][1]))
+        print("Manager SSN: " + emp_info[0][2])
+        print("Manager start date (YYYY-MM-DD): " + str(emp_info[0][3]))
+
+        conf = input("Are you sure you want to delete this department? ")
+
+        if conf == "No":
+            pass
+        else:
+            try:
+                # delete the department with that department number
+                cursor.execute("DELETE FROM Department WHERE Dnumber='%s'" % (dnumber))
+                print("Department successfully removed!")
+            except Error as err:
+                print(f"Error: '{err}'")
+                print("Please remove the necessary dependencies first!")
+    except:
+        return print("This department number does not exist!")
+
+    connection.commit()
+    cursor.close()
+
+def add_department_location():
+    cursor = connection.cursor()
+    
+    dnum = input("Enter department number? ")
+
+    # select all department locations for that department number and lock the row
+    cursor.execute("SELECT Dlocation FROM Dept_locations WHERE Dnumber='%s' FOR UPDATE" % (dnum))
+    loc_info = cursor.fetchall()
+
+    if len(loc_info) == 0:
+        return print("This department number does not exist!")
+    else:
+        loc_list = [loc[0] for loc in loc_info]
+        print("Department locations: " + ", ".join(loc_list))
+
+    new_loc = input("Enter new department location: ")
+
+    try:
+        # insert new location for that department number into table
+        cursor.execute("INSERT INTO Dept_locations VALUES ('%s', '%s')" % (dnum, new_loc))
+        connection.commit()
+        cursor.close()
+        return print("Department location successfully added!")
+    except Error as err:
+        print(f"Error: '{err}'")
+        print("This department location already exists!")
+    
+    connection.commit()
+    cursor.close()
+
+def remove_department_location():
+    cursor = connection.cursor()
+
+    dno = input("Enter the department number: ")
+
+    # select all department locations for that department number and lock the row(s)
+    cursor.execute("SELECT Dlocation FROM Dept_locations WHERE Dnumber='%s' FOR UPDATE" % (dno))
+    dept_loc = cursor.fetchall()
+
+    if len(dept_loc) == 0:
+        return print("This department number does not exist!")
+    else:
+        dept_loc_list = [loc[0] for loc in dept_loc]
+        print("Department locations: " +  ", ".join(dept_loc_list))
+        
+        rem_location = input("Which location do you want to remove? ")
+
+        if rem_location in dept_loc_list:
+            # delete the specified department location from that department
+            cursor.execute("DELETE FROM Dept_locations WHERE Dlocation ='%s' and Dnumber = '%s'" % (rem_location, dno))
+            print("Department location successfully removed!")
+        else:
+            print("This location is not a valid entry!")
+
+    connection.commit()
+    cursor.close()
+
+if __name__ == '__main__':
+    database = interface()
+    connection.close()
