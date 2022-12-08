cc31a900d1845f4acda93c581c5f6c48de55cf90
Michael Zheng <99830293+mzhengg@users.noreply.github.com>
2022-12-08 11:49:06 -0500

    changes

--- containers/interface/interface.py
+++ containers/interface/interface.py
@@ -15,28 +15,30 @@ connection = mysql.connector.connect(user=user, password=password, host=host, po
 
 def interface():
     i = 1
+
     while i != 0:
         print("Please select an operation to perform.")
         print()
         print("A - Quit Menu")
-        print("B - Add employee")
-        print("C - View employee")
-        print("D - Modify employee")
-        print("E - Remove employee")
-        print("F - Add dependent")
-        print("G - Remove dependent")
-        print("H - Add department")
-        print("I - View department")
-        print("J - Remove department")
-        print("K - Add department location")
-        print("L - Remove department location")
+        print("B - Add Employee")
+        print("C - View Employee")
+        print("D - Modify Employee")
+        print("E - Remove Employee")
+        print("F - Add Dependent")
+        print("G - Remove Dependent")
+        print("H - Add Department")
+        print("I - View Department")
+        print("J - Remove Department")
+        print("K - Add Department Location")
+        print("L - Remove Department Location")
         print()
 
         user_input = input("Enter the operation letter: ")
 
         if user_input == "A":
-            print("Closing menu")
+            print("Closing menu...")
             i = 0
+
         elif user_input == "B":
             add_employee()
 
@@ -66,46 +68,52 @@ def interface():
 
         elif user_input == "K":
             add_department_location()
-            
+
         elif user_input == "L":
             remove_department_location()
+
         else:
-            print("This is not a valid input")
-            i = 0
+            print("This is not a valid input letter.")
 
         print()
-        ans = input("Do you want to perform another operation? 'Yes' or 'No' ")
+        answer = input("Do you want to perform another operation? ('Yes' or 'No'): ")
 
-        if ans == "Yes":
+        if answer == "Yes":
             continue
-        elif ans == "No":
-            print("Closing menu")
+
+        elif answer == "No":
+            print("Closing menu...")
             i = 0
+
         else:
+            print("Not a valid input - closing menu...")
             i = 0
-            print("Not a valid input. Closing menu.")
 
 def add_employee():
     cursor = connection.cursor()
 
-    ssn = input("Enter employee SSN: ")
-    fname, minit, lname = input("Enter employee full name: (first, middle, last) ").split()
-    bdate = input("Enter employee birthdate: ")
-    address = input("Enter employee address: ")
-    sex = input("Enter employee sex: ")
-    salary = input("Enter employee salary: ")
-    super_ssn = input("Enter employee supervisor's SSN: (If none, then enter 'NULL') ")
-    
+    ssn = input("Enter the employee's SSN: ")
+    fname = input("Enter the employee's first name: ")
+    minit = input("Enter the employee's middle inital: ")
+    lname = input("Enter the employee's last name: ")
+    bdate = input("Enter the employee's birthdate: ")
+    address = input("Enter the employee's address: ")
+    sex = input("Enter the employee's sex: ")
+    salary = input("Enter the employee's salary: ")
+    dno = input("Enter the employee's department number: ")
+    super_ssn = input("Enter the employee's supervisor's SSN: (If none, then enter 'NULL') ")
+
     if super_ssn == 'NULL':
         super_ssn = None
 
-    dno = input("Enter the employee's department number: ")
-
     try:
         data = (fname, minit, lname, ssn, bdate, address, sex, salary, super_ssn, dno)
-        # insert new employee into table
-        cursor.execute("INSERT INTO Employee VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", data)
+
+        # add employee to 'Employee' table
+        cursor.execute("INSERT INTO EMPLOYEE VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)", data)
+
         print("Employee successfully added!")
+
     except Error as err:
         print(f"Error: '{err}'")
 
@@ -115,18 +123,18 @@ def add_employee():
 def view_employee():
     cursor = connection.cursor()
 
-    ssn = input("Enter employee SSN: ")
+    ssn = input("Enter the employee's SSN: ")
 
     # select all attributes of employee with that ssn
-    cursor.execute("SELECT * FROM Employee WHERE Ssn = '%s'" % (ssn))
-    emp_info = cursor.fetchall()
+    cursor.execute("SELECT * FROM EMPLOYEE WHERE SSN = '%s'" % (ssn))
+    employee_info = cursor.fetchall()
 
-    if len(emp_info) == 0:
-        return print("This employee does not exist!")
+    if len(employee_info) == 0:
+        return print("This employee doesn't exist!")
 
-    # select supervisor's full name
-    super_ssn = emp_info[0][8]
-    cursor.execute("SELECT Fname, Minit, Lname FROM Employee WHERE Ssn = '%s'" % (super_ssn))
+    # get supervisor's full name
+    super_ssn = employee_info[0][8]
+    cursor.execute("SELECT FNAME, MINIT, LNAME FROM EMPLOYEE WHERE SSN = '%s'" % (super_ssn))
     super_name = cursor.fetchall()
 
     if len(super_name) == 0:
@@ -135,36 +143,38 @@ def view_employee():
     else:
         super_name = " ".join(super_name[0])
 
-    # select department name
-    dno = emp_info[0][9]
-    cursor.execute("SELECT Dname FROM Department WHERE Dnumber = '%s'" % (dno))
+    # get department name
+    dno = employee_info[0][9]
+    cursor.execute("SELECT DNAME FROM DEPARTMENT WHERE DNUMBER = '%s'" % (dno))
     dname = cursor.fetchall()
 
-    # select dependent's name 
-    cursor.execute("SELECT Dependent_name FROM Dependent WHERE Essn = '%s'" % (ssn))
-    dep_names = cursor.fetchall()
+    # select dependents' names 
+    cursor.execute("SELECT DEPENDENT_NAME FROM DEPENDENT WHERE ESSN = '%s'" % (ssn))
+    dependent_names = cursor.fetchall()
 
     # display all the information
-    if len(dep_names) > 1:
-        dep_names = [entry[0] for entry in dep_names]
-        dep_names = ", ".join(dep_names)
-    elif len(dep_names) == 1:
-        dep_names = dep_names[0][0]
+    if len(dependent_names) > 1:
+        dependent_names = [entry[0] for entry in dependent_names]
+        dependent_names = ", ".join(dependent_names)
+
+    elif len(dependent_names) == 1:
+        dependent_names = dependent_names[0][0]
+
     else: 
-        dep_names = "NULL"
+        dependent_names = "NULL"
 
     print("Employee information: ")
-    print("Name: " + emp_info[0][0] + " " + emp_info[0][1] + " " + emp_info[0][2])
-    print("SSN: " + emp_info[0][3])
-    print("Birthdate: " + str(emp_info[0][4]))
-    print("Address: " + emp_info[0][5])
-    print("Sex: " + emp_info[0][6])
-    print("Salary: " + str(emp_info[0][7]))
-    print("Supervisor's SSN: " + super_ssn)
+    print("Name: " + employee_info[0][0] + " " + employee_info[0][1] + " " + employee_info[0][2])
+    print("SSN: " + employee_info[0][3])
+    print("Birthdate: " + str(employee_info[0][4]))
+    print("Address: " + employee_info[0][5])
+    print("Sex: " + employee_info[0][6])
+    print("Salary: " + str(employee_info[0][7]))
+    print("Supervisor SSN: " + super_ssn)
     print("Department number: " + str(dno))
     print("Supervisor name: " + super_name)
     print("Department name: " + dname[0][0])
-    print("Dependent names: ", dep_names)
+    print("Dependent names: " + dependent_names)
 
     connection.commit()
     cursor.close()
@@ -172,21 +182,21 @@ def view_employee():
 def modify_employee():
     cursor = connection.cursor()
 
-    ssn = input("Enter employee SSN: ")
+    ssn = input("Enter employee's SSN: ")
 
-    # select employee information and lock the record
-    cursor.execute("SELECT * FROM Employee WHERE Ssn = '%s' FOR UPDATE" % (ssn))
-    emp = cursor.fetchall()
+    # get employee information and lock the record
+    cursor.execute("SELECT * FROM EMPLOYEE WHERE SSN = '%s' FOR UPDATE" % (ssn))
+    employee_information = cursor.fetchall()
 
     print("Employee information: ")
-    print("Name: " + emp[0][0] + " " + emp[0][1] + " " + emp[0][2])
-    print("SSN: " + emp[0][3])
-    print("Birthdate: " + str(emp[0][4]))
-    print("Address: " + emp[0][5])
-    print("Sex: " + emp[0][6])
-    print("Salary: " + str(emp[0][7]))
-    print("Supervisor's SSN: " + str(emp[0][8]))
-    print("Department number: " + str(emp[0][9]))
+    print("Name: " + employee_information[0][0] + " " + employee_information[0][1] + " " + employee_information[0][2])
+    print("SSN: " + employee_information[0][3])
+    print("Birthdate: " + str(employee_information[0][4]))
+    print("Address: " + employee_information[0][5])
+    print("Sex: " + employee_information[0][6])
+    print("Salary: " + str(employee_information[0][7]))
+    print("Supervisor SSN: " + str(employee_information[0][8]))
+    print("Department number: " + str(employee_information[0][9]))
 
     i = 1
 
@@ -196,53 +206,67 @@ def modify_employee():
         print("B - Address")
         print("C - Sex")
         print("D - Salary")
-        print("E - Supervisor's SSN")
+        print("E - Supervisor SSN")
         print("F - Department number")
 
-        ans = input("Which attribute do you want to update? Input the appropriate letter: ")
+        answer = input("Which attribute do you want to modify? Enter the appropriate letter: ")
 
-        if ans == "A":
+        if answer == "A":
             return
-        elif ans == "B":
+
+        elif answer == "B":
             address = input("Enter the new address: ")
+
             # update employee's address to the new address
-            cursor.execute("UPDATE Employee SET address = '%s' WHERE ssn = '%s'" % (address, ssn))
+            cursor.execute("UPDATE EMPLOYEE SET ADDRESS = '%s' WHERE SSN = '%s'" % (address, ssn))
             print("Address successfully updated!")
-        elif ans == "C":
+
+        elif answer == "C":
             sex = input("Enter the new sex: ")
+
             # update employee's sex to the new sex 
-            cursor.execute("UPDATE Employee SET sex = '%s' WHERE ssn = '%s'" % (sex, ssn))
+            cursor.execute("UPDATE EMPLOYEE SET SEX = '%s' WHERE SSN = '%s'" % (sex, ssn))
             print("Sex successfully updated!")
-        elif ans == "D":
+
+        elif answer == "D":
             salary = input("Enter the new salary: ")
+
             # update employee's salary to the new salary 
-            cursor.execute("UPDATE Employee SET salary = '%s' WHERE ssn = '%s'" % (salary, ssn))
+            cursor.execute("UPDATE EMPLOYEE SET SALARY = '%s' WHERE SSN = '%s'" % (salary, ssn))
             print("Salary successfully updated!")
-        elif ans == "E":
+
+        elif answer == "E":
             super_ssn = input("Enter the new supervisor SSN: ")
+
             try:
-                # update employee's super_ssn to the new super_ssn
-                cursor.execute("UPDATE Employee SET Super_ssn = '%s' WHERE ssn = '%s'" % (super_ssn, ssn))
+                # update employee's supervisor ssn to the new supervisor ssn
+                cursor.execute("UPDATE EMPLOYEE SET SUPER_SSN = '%s' WHERE SSN = '%s'" % (super_ssn, ssn))
                 print("Supervisor SSN successfully updated!")
+
             except Error as err:
                 print(f"Error: '{err}'")
-        elif ans == "F":
+
+        elif answer == "F":
             dno = input("Enter the new department number: ")
+
             try:
-                # update employee dno to the new dno 
-                cursor.execute("UPDATE Employee SET dno = '%s' WHERE ssn = '%s'" % (dno, ssn))
+                # update employee department number to the new department number
+                cursor.execute("UPDATE EMPLOYEE SET DNO = '%s' WHERE SSN = '%s'" % (dno, ssn))
                 print("Department number successfully updated!")
+
             except Error as err:
                 print(f"Error: '{err}'")
+
         else:
-            return print("Error: Please input a valid option!")
+            print("This is not a valid input letter.")
         
         connection.commit()
-        inp = input("Are there any other attributes you want to update? Type 'Yes' or 'No' ")
+        answer = input("Do you want to perform another operation? ('Yes' or 'No'): ")
         
-        if inp == "Yes":
+        if answer == "Yes":
             continue
-        elif inp == "No":
+
+        elif answer == "No":
             i = 0
 
     cursor.close()
@@ -252,36 +276,40 @@ def remove_employee():
 
     ssn = input("Enter employee SSN: ")
 
-    # retrieve employee information with that ssn and lock the row
-    cursor.execute("SELECT * FROM Employee WHERE Ssn='%s' FOR UPDATE" % (ssn))
-    emp_info = cursor.fetchall()
+    # get employee information and lock the record
+    cursor.execute("SELECT * FROM EMPLOYEE WHERE SSN='%s' FOR UPDATE" % (ssn))
+    employee_information = cursor.fetchall()
 
     try:
         print("Employee information: ")
-        print("Name: " + emp_info[0][0] + " " + emp_info[0][1] + " " + emp_info[0][2])
-        print("SSN: " + emp_info[0][3])
-        print("Birthdate: " + str(emp_info[0][4]))
-        print("Address: " + emp_info[0][5])
-        print("Sex: " + emp_info[0][6])
-        print("Salary: " + str(emp_info[0][7]))
+        print("Name: " + employee_information[0][0] + " " + employee_information[0][1] + " " + employee_information[0][2])
+        print("SSN: " + employee_information[0][3])
+        print("Birthdate: " + str(employee_information[0][4]))
+        print("Address: " + employee_information[0][5])
+        print("Sex: " + employee_information[0][6])
+        print("Salary: " + str(employee_information[0][7]))
 
-        conf = input("Are you sure you want to delete this employee? Type 'Yes' or 'No'")
-        if conf == "No":
+        answer = input("Are you sure you want to delete this employee? Type 'Yes' or 'No': ")
+
+        if answer == "No":
             pass
+
         else:
             try:
-                # delete employee with the given ssn from employee table
-                cursor.execute("DELETE FROM Employee WHERE Ssn='%s'" % (ssn))
+                # delete employee from EMPLOYEE table
+                cursor.execute("DELETE FROM EMPLOYEE WHERE SSN='%s'" % (ssn))
                 print("Employee successfully removed!")
+
             except Error as err:
                 print(f"Error: '{err}'")
-                print("Please remove the necessary dependencies first!")
+                print("Please remove the dependents in the DEPENDENT table first!")
+
     except:
-        return print("This employee does not exist!")
+        return print("This employee doesn't exist!")
 
     connection.commit()
     cursor.close()
-    
+
 def add_dependent():
     cursor = connection.cursor()
 
@@ -419,12 +447,12 @@ def remove_department():
     try:
         # retrieve information about the department with that department number and lock the row
         cursor.execute("SELECT * FROM Department WHERE Dnumber='%s' FOR UPDATE" % (dnumber))
-        emp_info = cursor.fetchall()
+        employee_info = cursor.fetchall()
         print("Department information: ")
-        print("Department name: " + emp_info[0][0])
-        print("Department number: " + str(emp_info[0][1]))
-        print("Manager SSN: " + emp_info[0][2])
-        print("Manager start date (YYYY-MM-DD): " + str(emp_info[0][3]))
+        print("Department name: " + employee_info[0][0])
+        print("Department number: " + str(employee_info[0][1]))
+        print("Manager SSN: " + employee_info[0][2])
+        print("Manager start date (YYYY-MM-DD): " + str(employee_info[0][3]))
 
         conf = input("Are you sure you want to delete this department? ")
 
